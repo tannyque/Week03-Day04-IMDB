78b2ac04c2a0d68a8c710f473b55797aa4c41b0c
tannyque <tannyque1992@gmail.com>
2018-08-02 13:33:39 +0100

    Initial commit

---
+++ db/imdb.sql
@@ -0,0 +1,21 @@
+DROP TABLE castings;
+DROP TABLE stars;
+DROP TABLE movies;
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255)
+);
+
+CREATE TABLE stars(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
+
+CREATE TABLE castings(
+  id SERIAL PRIMARY KEY
+  movie_id INT REFERENCES movies(id),
+  star_id INT REFERENCES stars(id)
+);
